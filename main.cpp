f9aed59f5fc3e1cb80e0041eac78457aa2708e2e
Venkataram Edavamadathil Sivaram <vesion4690@gmail.com>
2024-07-10 23:44:07 -0700

    Handling branching control flow

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <cstdio>
 #include <cstring>
+#include <functional>
 #include <type_traits>
 #include <variant>
+#include <stack>
 #include <vector>
+#include <cassert>
 
 namespace op {
 
@@ -20,6 +23,7 @@ struct Global {
 
 struct PrimitiveType {
 	enum {
+		boolean,
 		i32,
 		f32,
 		vec4,
@@ -45,13 +49,25 @@ struct Store {
 	int src;
 };
 
+struct Cond {
+	int cond;
+	int failto;
+};
+
+struct Elif : Cond {};
+
+struct End {};
+
 using _general_base = std::variant <
 	Global,
 	PrimitiveType,
 	Primitive,
 	Construct,
 	List,
-	Store
+	Store,
+	Cond,
+	Elif,
+	End
 >;
 
 struct General : _general_base {
@@ -59,8 +75,6 @@ struct General : _general_base {
 };
 
 struct _dump_dispatcher {
-	op::General *pool;
-
 	void operator()(const Global &global) {
 		static const char *qualifier_table[] = {
 			"layout input",
@@ -71,26 +85,20 @@ struct _dump_dispatcher {
 			qualifier_table[global.qualifier], global.binding);
 	}
 
-	void operator()(const op::PrimitiveType &type) {
-		printf("type: ");
-		switch (type.type) {
-		case PrimitiveType::i32:
-			printf("i32");
-			break;
-		case PrimitiveType::f32:
-			printf("f32");
-			break;
-		case PrimitiveType::vec4:
-			printf("vec4");
-			break;
-		default:
-			printf("<?>");
-			break;
-		}
+	void operator()(const op::PrimitiveType &t) {
+		static const char *type_table[] = {
+			"bool", "int", "float", "vec4"
+		};
+
+		printf("type: %s", type_table[t.type]);
 	}
 
 	void operator()(const op::Primitive &p) {
-		printf("primitive: (%.2f, %.2f, %.2f, %.2f)", p.fdata);
+		printf("primitive: (%.2f, %.2f, %.2f, %.2f)",
+				p.fdata[0],
+				p.fdata[1],
+				p.fdata[2],
+				p.fdata[3]);
 	}
 
 	void operator()(const List &list) {
@@ -107,6 +115,21 @@ struct _dump_dispatcher {
 
 	void operator()(const op::Store &store) {
 		printf("store %%%d -> %%%d", store.src, store.dst);
+	}
+
+	void operator()(const op::Cond &cond) {
+		printf("cond %%%d -> %%%d", cond.cond, cond.failto);
+	}
+
+	void operator()(const op::Elif &elif) {
+		if (elif.cond >= 0)
+			printf("elif %%%d -> %%%d", elif.cond, elif.failto);
+		else
+			printf("elif (nil) -> %%%d", elif.failto);
+	}
+
+	void operator()(const End &) {
+		printf("end");
 	}
 
 	template <typename T>
@@ -128,28 +151,33 @@ struct IREmitter {
 	size_t size;
 	size_t pointer;
 
-	std::vector <int> main;
+	std::stack <int> control_flow_ends;
 
 	IREmitter() : pool(nullptr), size(0), pointer(0) {}
 
-	void reserve(size_t units) {
-		if (pointer + units > size) {
-			op::General *old = pool;
+	void resize(size_t units) {
+		op::General *old = pool;
 
-			size_t psize = size;
-			size = pointer + units;
-			pool = new op::General[size];
-
-			if (old) {
-				std::memcpy(pool, old, psize * sizeof(op::General));
-				delete[] old;
-			}
+		pool = new op::General[units];
+		if (old) {
+			std::memcpy(pool, old, size * sizeof(op::General));
+			delete[] old;
 		}
+
+		size = units;
 	}
 
 	int emit(const op::General &op) {
 		if (pointer >= size) {
+			if (size == 0)
+				resize(1 << 4);
+			else
+				resize(size << 2);
+		}
+
+		if (pointer >= size) {
 			printf("error, exceed global pool size, please reserve beforehand\n");
+			throw -1;
 			return -1;
 		}
 
@@ -157,20 +185,55 @@ struct IREmitter {
 		return pointer++;
 	}
 
-	int emit_main(const op::General &op) {
-		int i = emit(op);
-		main.push_back(i);
-		return i;
+	int emit(const op::Cond &cond) {
+		int p = emit((op::General) cond);
+		control_flow_ends.push(p);
+		return p;
+	}
+
+	int emit(const op::Elif &elif) {
+		int p = emit((op::General) elif);
+		assert(control_flow_ends.size());
+		auto ref = control_flow_ends.top();
+		control_flow_ends.pop();
+		control_flow_callback(ref, p);
+		control_flow_ends.push(p);
+		return p;
+	}
+
+	int emit(const op::End &end) {
+		int p = emit((op::General) end);
+		assert(control_flow_ends.size());
+		auto ref = control_flow_ends.top();
+		control_flow_ends.pop();
+		control_flow_callback(ref, p);
+		return p;
+	}
+
+	void control_flow_callback(int ref, int p) {
+		auto &op = pool[ref];
+		if (std::holds_alternative <op::Cond> (op)) {
+			std::get <op::Cond> (op).failto = p;
+		} else if (std::holds_alternative <op::Elif> (op)) {
+			std::get <op::Elif> (op).failto = p;
+		} else {
+			printf("op not conditional, is actually:");
+			std::visit(op::_dump_dispatcher(), op);
+			printf("\n");
+			assert(false);
+		}
 	}
 
 	void dump() {
-		for (size_t i = 0; i < size; i++) {
+		printf("GLOBALS (%4d/%4d)\n", pointer, size);
+		for (size_t i = 0; i < pointer; i++) {
 			printf("[%4d]: ", i);
-			std::visit(op::_dump_dispatcher { pool }, pool[i]);
+			std::visit(op::_dump_dispatcher(), pool[i]);
 			printf("\n");
 		}
 	}
 
+	// TODO: one per thread
 	static IREmitter active;
 };
 
@@ -213,8 +276,6 @@ struct layout_out : gltype <T> {
 	void operator=(const T &t) {
 		auto &em = IREmitter::active;
 
-		em.reserve(3);
-
 		printf("op= from tag=%d\n", this->tag);
 		// TODO: check if t already has an active iseq
 
@@ -230,7 +291,7 @@ struct layout_out : gltype <T> {
 		store.dst = em.emit(qualifier);
 		store.src = t.synthesize();
 
-		em.emit_main(store);
+		em.emit(store);
 	}
 };
 
@@ -248,8 +309,6 @@ struct vec : gltype <T[N]> {
 
 		auto &em = IREmitter::active;
 
-		em.reserve(1 + N);
-
 		op::PrimitiveType t;
 		t.type = op::PrimitiveType::vec4;
 
@@ -266,13 +325,22 @@ struct vec : gltype <T[N]> {
 		ctor.args = em.emit(l);
 
 		// TODO: emit and cache in one
-		return em.emit_main(ctor);
+		return em.emit(ctor);
 	}
 };
 
 using vec4 = vec <float, 4>;
 
-struct boolean : gltype <bool> {};
+struct boolean : gltype <bool> {
+	int synthesize() {
+		auto &em = IREmitter::active;
+
+		op::PrimitiveType t;
+		t.type = op::PrimitiveType::f32;
+
+		return em.emit(t);
+	}
+};
 
 template <typename T>
 boolean operator==(const gltype <T> &A, const gltype <T> &B)
@@ -288,17 +356,50 @@ boolean operator==(const gltype <T> &A, const U &B)
 	return (A == gltype <T> (B));
 }
 
-// cond(...) for if
-// elif(...) and elif() for else if and else
-// loop(...) for while and auto v = loop(init, cond, step) for for
-// end() to end all control flow
+// Branching emitters
+void cond(boolean b)
+{
+	auto &em = IREmitter::active;
+	op::Cond branch;
+	branch.cond = b.synthesize();
+	em.emit(branch);
+}
+
+void elif(boolean b)
+{
+	auto &em = IREmitter::active;
+	op::Elif branch;
+	branch.cond = b.synthesize();
+	em.emit(branch);
+}
+
+void elif()
+{
+	// Treated as an else
+	auto &em = IREmitter::active;
+	op::Elif branch;
+	branch.cond = -1;
+	em.emit(branch);
+}
+
+void end()
+{
+	auto &em = IREmitter::active;
+	em.emit(op::End());
+}
 
 void fragment_shader()
 {
-	layout_in <int, 0> cond;
+	layout_in <int, 0> flag;
 	layout_out <vec4, 0> fragment;
 
-	fragment = vec4(1.0);
+	cond(flag == 0);
+		fragment = vec4(1.0);
+	elif(flag == 1);
+		fragment = vec4(0.5);
+	elif();
+		fragment = vec4(0.1);
+	end();
 }
 
 int main()
